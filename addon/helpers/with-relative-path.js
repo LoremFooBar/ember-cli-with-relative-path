0cbe2653e7d3e39878ac237d220b495f77a5bfab
Theodor Storm Kristensen <tstormk@gmail.com>
2019-05-03 11:39:45 +0200

    Add support for Ember versions below 2.16

--- addon/helpers/with-relative-path.js
+++ addon/helpers/with-relative-path.js
@@ -6,11 +6,12 @@ import { isPresent } from '@ember/utils';
 
 export default Helper.extend({
   router: service(),
+  routing: service('-routing'),
 
   compute([routeName], hash) {
     assert('with-relative-path: Must provide a routeName', isPresent(routeName));
 
-    let currentRouteName = get(this, 'router.currentRouteName');
+    let currentRouteName = this.getCurrentRouteName();
     let replaceIndex = hash.replaceIndex === undefined ? true : hash.replaceIndex;
 
     if (replaceIndex && currentRouteName.endsWith('index')) {
@@ -18,5 +19,19 @@ export default Helper.extend({
     }
 
     return `${currentRouteName}.${routeName}`;
+  },
+
+  getCurrentRouteName() {
+    let router = get(this, 'router');
+
+    /* For Ember versions below 2.16, the RouterService isn't publicly available.
+       So if `router` is empty, we assume that Ember is < 2.16, and instead try
+       to get the `currentRouteName` in a more hacky way. */
+
+    if (!router) {
+      return get(this, 'routing.router.currentRouteName');
+    }
+
+    return get(this, 'router.currentRouteName');
   }
 });
