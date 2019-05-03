b9f6f6ea4ea0388ec98ab504a975c0f936086c58
Theodor Kristensen <theodor.kristensen@C02YD03AJG5L.local>
2019-05-03 09:17:03 +0200

    Assert that routeName is provided

--- addon/helpers/with-relative-path.js
+++ addon/helpers/with-relative-path.js
@@ -1,11 +1,15 @@
 import Helper from '@ember/component/helper';
 import { get } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { assert } from '@ember/debug';
+import { isPresent } from '@ember/utils';
 
 export default Helper.extend({
   router: service(),
 
   compute([routeName], hash) {
+    assert('with-relative-path: Must provide a routeName', isPresent(routeName));
+
     let currentRouteName = get(this, 'router.currentRouteName');
     let replaceIndex = hash.replaceIndex === undefined ? true : hash.replaceIndex;
 
